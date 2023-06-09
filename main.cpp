2f6d9e94177e0c9e7a14ef2ed3b9b1b6a8a2cbc4
Maheem <88165826+maheem20@users.noreply.github.com>
2023-06-09 21:33:56 +0000

    initial commit
    include necessary libraries and define initial constants

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+const int width = 20;   // Width of the game board
+const int height = 20;  // Height of the game board
+int x, y;               // Coordinates of the snake's head
+int fruitX, fruitY;     // Coordinates of the fruit
+int score;              // Player's score
+bool gameOver;          // Game over flag
+
+enum Direction { STOP = 0, LEFT, RIGHT, UP, DOWN };
+Direction dir;          // Current direction of the snake
